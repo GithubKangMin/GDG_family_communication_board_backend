c99f1d4cfe0280bfeec498131302534fb87dd49c
GithubKangMin <leelucky1@naver.com>
2024-11-03 02:42:07 +0900

    작동 확인 완료

--- src/main/java/family/family_communication_board/FamilyCommunicationBoardApplication.java
+++ src/main/java/family/family_communication_board/FamilyCommunicationBoardApplication.java
@@ -2,8 +2,10 @@ package family.family_communication_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "family")
 public class FamilyCommunicationBoardApplication {
 
 	public static void main(String[] args) {
